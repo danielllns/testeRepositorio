7207ca8d1b71fa950648f03e643d2ac568e6ced0
Fabio Morcillo do Nascimento <fabio_morc@hotmail.com>
2015-02-24 00:15:39 -0300

    Fabio

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		// e ae a e a ea e xD
+		
 		System.out.println("Hello World!");
 	}
 
